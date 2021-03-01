ba505b2eeb9fa00f959923b435b08d264bcf6c30
Louis Amstutz <louis34@gmail.com>
2021-02-28 20:51:27 -0500

    Add Oauth authorization

--- client/js/main.js
+++ client/js/main.js
@@ -1,15 +1,33 @@
 const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit', hour: 'numeric', minute: 'numeric' });
 
-function getData() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var items = JSON.parse(this.responseText);
-            addItem(items);
+function init() {
+    makeXhrRequest("GET", "isLoggedIn", (response) => {
+        if (response.isLoggedIn) {
+            const loginControls = document.getElementById("login-controls");
+            loginControls.className = "hidden";
+            const artistInfo = document.getElementById("artist-info");
+            artistInfo.className = "";
         }
-    };
-    xhr.open("GET", "items", true);
-    xhr.send();
+    });
+}
+
+function login() {
+    makeXhrRequest("GET", "albums", (response) => {
+        console.log(response);
+    });
+}
+
+function authNoLogin() {
+    makeXhrRequest("GET", "authNoLogin", (response) => {
+        console.log(response);
+    });
+}
+
+function searchArtists() {
+    const artistSearch = document.getElementById("artist-search");
+    makeXhrRequest("GET", "artistSearch?q=" + artistSearch.value, (response) => {
+        console.log(response);
+    });
 }
 
 function addItem(items) {
@@ -20,4 +38,29 @@ function addItem(items) {
         li.innerText = item.author + ":   " + item.en;
         list.appendChild(li);
     }
+}
+
+function getAlbums() {
+    const xhr = new XMLHttpRequest();
+    const spotifyUrl = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10";
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var items = JSON.parse(this.responseText);
+            addItem(items);
+        }
+    };
+    xhr.open("GET", spotifyUrl, true);
+    xhr.send();
+}
+
+function makeXhrRequest(method, url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(this.responseText);
+            callback(response);
+        }
+    };
+    xhr.open(method, url, true);
+    xhr.send();
 }
--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,113 @@
 import express = require('express');
 import path = require('path');
 import https = require("https");
+import fs = require('fs');
+import crypto = require("crypto");
+import querystring = require('querystring');
+import cookieParser = require('cookie-parser');
+import OAuth = require('oauth');
+const fetch = require('node-fetch');
 
-const progQuotesUrl = "https://programming-quotes-api.herokuapp.com/";
+const progQuotesUrl = 'https://programming-quotes-api.herokuapp.com/';
+const loginUri = 'https://accounts.spotify.com/authorize?';
+const baseAuthUri = 'https://accounts.spotify.com/';
+const spotifyApiUrl = "https://api.spotify.com";
+const redirectUri = 'http://localhost:3000/callback';
 
 // Create a new express app instance
 const app: express.Application = express();
 
-app.use(express.static('client'));
+let clientSecret: string;
+let clientId: string;
+let accessToken: string;
+const stateKey = 'spotify_auth_state';
+
+app.use(express.static('client'))
+   .use(cookieParser());
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
+app.get('/isLoggedIn', (req: express.Request, res: express.Response) => {
+    const response = {
+        isLoggedIn: (accessToken !== undefined)
+    };
+    res.status(200);
+    res.setHeader('Content-type', 'text/json');
+    res.send(JSON.stringify(response));
+});
+
+app.get('/login', (req: express.Request, res: express.Response) => {
+    const state = crypto.randomBytes(8).toString('hex');
+    res.cookie(stateKey, state);
+  
+    const scope = 'user-read-private user-read-email';
+    res.redirect(loginUri +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectUri,  
+        state: state
+      })
+    );
+});
+
+app.get('/callback',  (req: express.Request, res: express.Response) => {
+    authorize(<string>req.query.code, true, () => {
+        res.sendFile(path.join(__dirname + '/../client/index.html'));
+    });
+});
+
+app.get('/authNoLogin',  (req: express.Request, res: express.Response) => {
+    authorize('', false, () => {
+        res.sendFile(path.join(__dirname + '/../client/index.html'));
+    });
+});
+
+function authorize(code: string, isAuthCode: boolean, callback: () => void) {
+    const grantType = isAuthCode ? 'authorization_code' : 'client_credentials';
+    const oauth2 = new OAuth.OAuth2(
+        clientId,
+        clientSecret, 
+        baseAuthUri, 
+        undefined,
+        'api/token', 
+        undefined
+    );
+    oauth2.getOAuthAccessToken(
+        code,
+        {
+           'grant_type': grantType,
+           'redirect_uri': redirectUri
+        },
+        (e, access_token, refresh_token, results) => {
+            accessToken = access_token;
+            console.log(accessToken, refresh_token, results);
+            callback();
+        }
+     );
+}
+
+app.get('/artistSearch', async (req: express.Request, res: express.Response) => {
+    const response = await fetch(spotifyApiUrl + '/v1/search?type=artist&q=' + req.query.q, {
+        method: 'get',
+        headers: {'Authorization': 'Bearer ' + accessToken}
+    });
+    const data = await response.json();
+
+    res.status(200);
+    res.setHeader('Content-type', 'text/json');
+    res.send(JSON.stringify(data));
+});
+
 app.get('/items', (req: express.Request, res: express.Response) => {  
     const url = progQuotesUrl + "quotes";
     
-    https.get(url, (res2) => {
+    https.get(url, (res2: any) => {
         let data = "";
-        res2.on('data', (chunk) => {
+        res2.on('data', (chunk: any) => {
             data += chunk;
         });
         res2.on('end', () => {
@@ -38,5 +127,13 @@ app.get('/items', (req: express.Request, res: express.Response) => {
   
 
 app.listen(3000, () => {
+    fs.readFile('client-info.json', 'utf8', (err, data) => {
+        if (err) {
+          return console.log(err);
+        }
+        const jsonData = JSON.parse(data);
+        clientId = jsonData['client_id'];
+        clientSecret = jsonData.client_secret;
+    });
     console.log('App is listening on port 3000!');
 });
